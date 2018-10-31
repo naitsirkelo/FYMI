45d4e98350704e8a5f77e67bdf7f0ab46325d378
OleKL <oleklar@stud.ntnu.no>
2018-10-31 15:08:22 +0100

    Deploy

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import(
+  "fmt"
+  "os"
+  "log"
+  "net/http"
+)
+
+ - - - - - - - - - -
+
+func GetPort() string {
+	 	var port = os.Getenv("PORT")
+ 				// Port sets to :8080 as a default
+ 		if (port == "") {
+ 			port = "8080"
+			fmt.Println("No PORT variable detected, defaulting to " + port)
+ 		}
+ 		return (":" + port)
+}
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello Internet!")
+}
+
+ - - - - - - - - - -  main  - - - - - - - - - - - - - 
+
+func main() {
+  fmt.Println("Hello World!")
+
+  http.HandleFunc("/", handler)
+
+  err := http.ListenAndServe(GetPort(), nil)
+  if err != nil {
+      log.Fatal("ListenAndServe Error: ", err)
+  }
+}
+
+ - - - - - - - - - - - - - - - - - - - - - - - - - - -
