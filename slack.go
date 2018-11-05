6f6bfd5768e97d7a3228e39bb99500131266472f
marhaako2 <marhaako@stud.ntnu.no>
2018-11-05 16:49:21 +0100

    fixed bug where payload wouldnt send correctly

--- slack.go
+++ slack.go
@@ -2,15 +2,15 @@ package main
 
 import(
 	"bytes"
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 var WebhookUrl string = "https://hooks.slack.com/services/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"
 
 func SendPayload(payload string) error {
-	fmt.Println(payload)
-	var jsonStr = []byte(`{"text": payload}`)
+	val := map[string]string{"text": payload}
+	jsonStr, err := json.Marshal(val)
 	req, err := http.NewRequest("POST", WebhookUrl, bytes.NewBuffer(jsonStr))
 	req.Header.Set("Content-Type", "application/json")
 
