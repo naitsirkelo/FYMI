3033b4f2499a00abfdec2119d93757f379d347d1
marhaako2 <marhaako@stud.ntnu.no>
2018-11-06 15:32:01 +0100

    Finished payload struct

--- handler.go
+++ handler.go
@@ -9,7 +9,19 @@ import(
 
 
 type Payload struct {
+	Token string `schema:"token"`
+	TeamId string `schema:"&team_id"`
+	TeamDomain string `schema:"&team_domain"`
+	EnterpriseId string `schema:"&enterprise_id"`
+	EnterpriseName string `schema:"&enterprise_name"`
+	ChannelId string `schema:"&channel_id"`
+	ChannelName string `schema:"&channel_name"`
+	UserId string `schema:"&user_id"`
+	UserName string `schema:"&user_name"`
+	Command string `schema:"&command"`
 	Text string `schema:"&text"`
+	ResponseUrl string `schema:"&response_url"`
+	TriggerId string `schema:"&trigger_id"`
 }
 
 func IdHandler(w http.ResponseWriter, r *http.Request) {
