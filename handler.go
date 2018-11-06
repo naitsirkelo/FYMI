969ade7fe3eb882c12ac0aeefc840f118be0841d
marhaako2 <marhaako@stud.ntnu.no>
2018-11-06 16:34:08 +0100

    T

--- handler.go
+++ handler.go
@@ -43,7 +43,7 @@ func IdHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}*/
 
-	fmt.Fprintf(w, "%v as %t\n", r.Form["text"], r.Form["text"])
+	fmt.Fprintf(w, "%v as %T\n", r.Form["text"], r.Form["text"])
 //	SendPayload(payload.Text)
 }
 func BotHandler(w http.ResponseWriter, r *http.Request) {
